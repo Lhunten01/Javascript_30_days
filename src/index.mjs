20b2934021db6687d70f91af8ac3cc6bc9de68f1
Tenzin Lhundup <120653962+Lhunten01@users.noreply.github.com>
2024-05-22 14:33:26 +0530

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,36 @@
+let word = "challenge";
+let word2 = " 30 days of challenge";
+// console.log(word + word2);
+word = word.concat(word2);
+console.log(word.length);
+console.log(word.toLowerCase());
+console.log(word.toUpperCase());
+console.log(word.substring(0, 10));
+console.log(word.substr(0, 10));
+console.log(word2.length);
+console.log(word2.substr(4, 18));
+console.log(word2.charAt(5));
+console.log(word2.charCodeAt("d"));
+console.log(word2.lastIndexOf("a"));
+console.log(word2.indexOf("a"));
+console.log(word2.trim(" "));
+console.log(
+  "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another."
+);
+console.log(
+  "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same \t with charity you give love, so don't just give money but reach out your hand instead."
+);
+let num = "10";
+let num1 = "9.6";
+console.log(num === 10);
+let x = parseInt(num);
+let z = parseFloat(num1);
+console.log(typeof num1);
+console.log(typeof z);
+let y = parseInt(num1);
+console.log(typeof y);
+console.log(Math.floor(Math.random() * 20));
+
+//The Day 2 javascript 
+
+
